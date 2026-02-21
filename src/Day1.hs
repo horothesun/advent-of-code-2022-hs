dfe53d0715d3ee918fc53eae83d88bd167c23688
horothesun <horothesun@gmail.com>
2026-02-21 18:09:13 +0000

    Day 1: WIP

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,12 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Day1 where
 
 import qualified Data.Text as T
-import qualified Data.Text.IO as T
+import qualified Data.Text.Read as TR
+import Data.List (sort)
+import Data.List.Split (splitOn)
+import Data.Semigroup (Sum (..))
 
-data Answer = Answer deriving (Eq, Show)
+newtype Calories = Calories Int deriving (Eq, Show)
+newtype FoodItem = FoodItem Calories deriving (Eq, Show)
+newtype ElfPack = ElfPack [FoodItem] deriving (Eq, Show)
 
-program :: T.Text -> IO ()
-program = print . logic
+totalCalories :: ElfPack -> Int
+totalCalories (ElfPack foodItems) = getSum $ foldMap getCalories foodItems
+ where
+  getCalories :: FoodItem -> Sum Int
+  getCalories (FoodItem (Calories c)) = Sum c
 
-logic :: T.Text -> Answer
-logic = const Answer
+getElfWithMostCaloriesTotalCalories :: [T.Text] -> Maybe Int
+getElfWithMostCaloriesTotalCalories input = aux <$> parseElfPacks input
+  where
+    aux :: [ElfPack] -> Int
+    aux eps = sum $ (take 3) $ reverse $ sort (totalCalories <$> eps)
+
+parseFoodItem :: T.Text -> Maybe FoodItem
+parseFoodItem t = case TR.decimal t of
+  Right (n, rest)
+    | T.null rest -> Just $ FoodItem $ Calories n
+    | otherwise -> Nothing
+  Left _ -> Nothing
+
+parseElfPack :: [T.Text] -> Maybe ElfPack
+parseElfPack items = ElfPack <$> traverse parseFoodItem items
+
+parseElfPacks :: [T.Text] -> Maybe [ElfPack]
+parseElfPacks input = traverse parseElfPack $ splitOn [""] input
--- test/Day1Spec.hs
+++ test/Day1Spec.hs
@@ -1,16 +1,72 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Day1Spec where
 
+import qualified Data.Text as T
 import Day1
 import FileLoader
+import NeatInterpolation (trimming)
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck
 
 spec :: Spec
 spec = describe "Day 1" $ do
-  prop "" $
-    \l -> reverse (reverse l) == (l :: [Int])
-
   it "load big input from file" $ do
-    input <- getLinesFromFile "test/day1_input.txt"
+    input <- bigInput
     length input `shouldBe` 2223
+
+  it "parse small input" $
+    parseElfPacks smallInput `shouldBe` Just [
+      ElfPack [
+        FoodItem $ Calories 1000,
+        FoodItem $ Calories 2000,
+        FoodItem $ Calories 3000
+      ],
+      ElfPack [
+        FoodItem $ Calories 4000
+      ],
+      ElfPack [
+        FoodItem $ Calories 5000,
+        FoodItem $ Calories 6000
+      ],
+      ElfPack [
+        FoodItem $ Calories 7000,
+        FoodItem $ Calories 8000,
+        FoodItem $ Calories 9000
+      ],
+      ElfPack [
+        FoodItem $ Calories 10000
+      ]
+    ]
+
+  it "total Calories carried by the Elf with most calories (small input)" $
+    getElfWithMostCaloriesTotalCalories smallInput `shouldBe` Just 24000
+
+  it "total Calories carried by the Elf with most calories (BIG input)" $ do
+    input <- bigInput
+    getElfWithMostCaloriesTotalCalories input `shouldBe` Just 70764
+
+bigInput :: IO [T.Text]
+bigInput = getLinesFromFile "test/day1_input.txt"
+
+smallInput :: [T.Text]
+smallInput =
+  T.splitOn
+    "\n"
+    [trimming|
+      1000
+      2000
+      3000
+
+      4000
+
+      5000
+      6000
+
+      7000
+      8000
+      9000
+
+      10000
+      |]
